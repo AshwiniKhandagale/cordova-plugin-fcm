8c767912893e9f06718d6fde7e1f7038ecd02610
Ashwini Khandagale <ashwini.khandagale@virgoinnovation.com>
2020-02-20 19:50:33 +0530

    minor changes add

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package com.gae.scaffolder.plugin;
 
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
